70833a232763656cdbabbc85146f738e289efc0d
Katie Owen <katherinehowen@gmail.com>
2015-12-01 21:07:49 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
       alert('You clicked me!')
   });
 });
+
+// window.onload = function() {
+//   alert(1);
+//   document.getElementById('clickable').onclick = function() {
+//     alert('You clicked me!');
+//   }
+// }
+
